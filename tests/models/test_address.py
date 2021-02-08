aa035fb58e73dabf5846ed90e544a0744d7d8a0e
Clara Baumgarten <clara.odebrecht@olist.com>
2021-02-08 13:50:42 -0300

    Alterando nome

--- tests/models/test_address.py
+++ tests/models/test_address.py
@@ -1,5 +1,5 @@
 import pytest
-from models.state import State
+from models.address import Address
 
 
 class TestState:
@@ -16,28 +16,28 @@ class TestState:
 
     def test_address_name_len():
         with pytest.raises(ValueError):
-            address = State('rua amintas'*100, 453, '34352-54')
+            address = Address('rua amintas'*100, 453, '34352-54')
 
     def test_address_name_int():
         with pytest.raises(TypeError):
-            address = State(10)
+            address = Address(10)
 
     def test_address_number_empty():
         with pytest.raises(ValueError):
-            address = State('rua amintas', None, '34352-54')
+            address = Address('rua amintas', None, '34352-54')
 
     def test_address_number_str():
         with pytest.raises(TypeError):
-            address = State('rua amintas', '453', '34352-54')
+            address = Address('rua amintas', '453', '34352-54')
 
     def test_address_description_empty():
         with pytest.raises(ValueError):
-            address = State('rua amintas', 564, '')
+            address = Address('rua amintas', 564, '')
 
     def test_address_description_len():
         with pytest.raises(ValueError):
-            address = State('rua amintas', 453, '34352-54'*100)
+            address = Address('rua amintas', 453, '34352-54'*100)
 
     def test_address_description_int():
         with pytest.raises(TypeError):
-            address = State('rua amintas', 453, 100)
+            address = Address('rua amintas', 453, 100)
