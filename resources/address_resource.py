759bd46512b8a5142e089194386fe161fad2dfbc
Regina Beretta <reginabmre@gmail.com>
2021-02-09 09:12:08 -0400

    adding resources

---
+++ resources/address_resource.py
@@ -0,0 +1,36 @@
+from flask_restful import fields, marshal_with
+from dao.address_dao import AddressDao
+from models.address import Address 
+from resources.base_resource import BaseResource
+
+
+class AddressResource(BaseResource):
+    fields = {
+        "id_": fields.Integer,
+        "description": fields.String,
+        "number": fields.Integer,
+        "cep": fields.String,
+        "id_district": fields.Integer
+    }
+
+    def __init__(self):
+        self.__dao = AddressDao()
+        self.__model_type = Address
+
+        super().__init__(self.__dao, self.__model_type)
+
+    @marshal_with(fields)
+    def get(self, id=None):
+        return super().get(id)
+
+    @marshal_with(fields)
+    def post(self):
+        return super().post()
+
+    @marshal_with(fields)
+    def put(self, id):
+        return super().put(id)
+
+    @marshal_with(fields)
+    def delete(self, id):
+        return super().delete(id)
---
+++ resources/city_resource.py
@@ -0,0 +1,34 @@
+from flask_restful import fields, marshal_with
+from dao.city_dao import CityDao
+from models.city import City 
+from resources.base_resource import BaseResource
+
+
+class CityResource(BaseResource):
+    fields = {
+        "id_": fields.Integer,
+        "description": fields.String,
+        "id_state": fields.Integer
+    }
+
+    def __init__(self):
+        self.__dao = CityDao()
+        self.__model_type = City
+
+        super().__init__(self.__dao, self.__model_type)
+
+    @marshal_with(fields)
+    def get(self, id=None):
+        return super().get(id)
+
+    @marshal_with(fields)
+    def post(self):
+        return super().post()
+
+    @marshal_with(fields)
+    def put(self, id):
+        return super().put(id)
+
+    @marshal_with(fields)
+    def delete(self, id):
+        return super().delete(id)
